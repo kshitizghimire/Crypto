72becdb008050ea8ef36a729c0b4918377061141
Tiz <kshitiz.ghimire@gmail.com>
2021-06-11 17:00:38 +1000

    add coins list

--- Crypto/ContentView.swift
+++ Crypto/ContentView.swift
@@ -7,10 +7,37 @@
 
 import SwiftUI
 
+
+struct Coin: Codable {
+    var id: String
+    var symbol: String
+    var name: String
+}
+
 struct ContentView: View {
+    
+    @State var coins: [Coin] = []
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(coins, id: \.id) { coin in
+            VStack(alignment: .leading) {
+                Text(coin.name)
+                    .font(.headline)
+                Text(coin.symbol)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        let url = URL(string: "https://api.coingecko.com/api/v3/coins/list")!
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data,
+               let decodedResponse = try? JSONDecoder().decode([Coin].self, from: data) {
+                self.coins = decodedResponse
+            }
+            
+        }.resume()
     }
 }
 
@@ -19,3 +46,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
