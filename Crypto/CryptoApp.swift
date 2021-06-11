f7e53fbbbe765ddc89dc20c04430ae269e3ee981
Tiz <kshitiz.ghimire@gmail.com>
2021-06-11 16:39:35 +1000

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Tiz on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
